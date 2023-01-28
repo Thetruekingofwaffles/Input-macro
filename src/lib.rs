77dcdb855de059ce95960ac77a03d845e88dc88b
Thetruekingofwaffles <thetruekingofwaffles@gmail.com>
2023-01-28 16:52:10 -0600

    It works pretty good, but still strange error without 'use std::io::Write' in main

---
+++ src/lib.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::io::Write;
+
+
+#[macro_export]
+macro_rules! input{
+    ($a:expr) =>{
+       {
+        
+        print!("{}", $a);
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        input
+       }
+    };
+
+   (_) =>{{
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        
+        input
+        }
+    };
+
+
+}
+
